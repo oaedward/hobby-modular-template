1e2825c8dcaf9453052c7c6b9b1c5e7c47915a9a
Edward Opoku-Agyemang <87245788+oaedward@users.noreply.github.com>
2023-12-06 15:22:25 +0000

    Initial commit

---
+++ src/pages/IndexPage/IndexPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <q-page
+    padding
+    class="column flex-center"
+  >
+    <h1
+      class="text-weight-thin text-blue-grey"
+      style="font-size: 4em;"
+    >
+      You Can Build Anything...
+    </h1>
+  </q-page>
+</template>
